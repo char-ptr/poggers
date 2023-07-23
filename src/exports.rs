16292919780475de2d5662cb3f051daf5c697ccf
Autist69420 <56086819+Autist69420@users.noreply.github.com>
2023-07-23 15:55:00 +0200

    fix: Export `HMODULE` from poggers to be used for poggers-derive

--- src/exports.rs
+++ src/exports.rs
@@ -1,7 +1,7 @@
 /// this entire module is only used for [poggers_derive::create_entry]
 #[cfg(windows)]
 pub use windows::Win32::{
-    Foundation::{BOOL, HANDLE},
+    Foundation::{BOOL, HANDLE, HMODULE},
     System::{
         Console::{AllocConsole, FreeConsole},
         SystemServices::DLL_PROCESS_ATTACH,
