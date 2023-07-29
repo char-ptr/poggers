3c43595e60f277cf2b164879386718a1df5d8024
aixeria <44528100+pozm@users.noreply.github.com>
2023-07-30 00:14:29 +0100

    add linux internals

--- src/structures/process/implement/linux/internal.rs
+++ src/structures/process/implement/linux/internal.rs
@@ -1,34 +1,51 @@
-use crate::{traits::Mem, structures::process::{Process, Internal}, sigscan::SigScan};
+use crate::{traits::Mem, structures::{process::{Process, Internal}, protections::Protections}, sigscan::SigScan};
 
 impl Mem for Process<Internal> {
     unsafe fn alter_protection(&self,addr:usize, size: usize, prot: crate::structures::protections::Protections) -> Result<crate::structures::protections::Protections,crate::traits::MemError> {
-        todo!()
+        let result = libc::mprotect(addr as *mut libc::c_void, size, prot.native());
+        if result == -1 {
+            Err(crate::traits::MemError::ProtectFailure(addr, size, prot))
+        } else {
+            Ok(Protections::from_native(result))
+        }
     }
 
     unsafe fn raw_read(&self, addr: usize,data: *mut u8, size: usize) -> Result<(),crate::traits::MemError> {
-        todo!()
+        (addr as *mut u8).copy_to_nonoverlapping(data, size);
+        Ok(())
+
     }
 
     unsafe fn raw_write(&self, addr: usize,data: *const u8, size: usize) -> Result<(),crate::traits::MemError> {
-        todo!()
+        (addr as *mut u8).copy_from_nonoverlapping(data, size);
+        Ok(())
     }
 
     unsafe fn raw_virtual_alloc(&self, addr:Option<usize>, size:usize, prot: crate::structures::protections::Protections) -> Result<usize,crate::traits::MemError> {
-        todo!()
+        let addr_or_null = addr.map(|x| x as *mut libc::c_void ).unwrap_or(std::ptr::null_mut());
+        let to_proc = "/proc/self/maps";
+
+        let fder = libc::open(to_proc.as_ptr() as *const i8, libc::O_RDONLY, 0);
+
+        let addr = libc::mmap(addr_or_null, size, prot.native(), libc::MAP_PRIVATE, fder, 0);
+
+        Ok(addr as usize)
     }
 
     unsafe fn raw_virtual_free(&self, addr:usize, size:usize) -> Result<(),crate::traits::MemError> {
-        todo!()
+        libc::munmap(addr as *mut libc::c_void, size);
+        Ok(())
     }
 }
 impl Process<Internal> {
-    pub fn new() -> Self {
-        todo!()
-    }
-}
-impl Default for Process<Internal> {
-    fn default() -> Self {
-        Self::new()
+    pub(crate) fn new() -> Self {
+        let name = std::fs::read_to_string("/proc/self/comm").unwrap();
+        Self {
+            pid: unsafe { libc::getpid() } as u32,
+            name,
+            handl: None,
+            mrk: Default::default(),
+        }
     }
 }
 impl SigScan for Process<Internal> {}
