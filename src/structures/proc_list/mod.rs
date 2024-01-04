4694b336b97fd6acadf6b8bbaf4cd607090e10c0
ElonGates <59313695+VilotStar@users.noreply.github.com>
2024-01-04 18:00:45 +0000

    temp fix for proc_list mod.rs
    Poggers will only compile for target macos otherwise

--- src/structures/proc_list/mod.rs
+++ src/structures/proc_list/mod.rs
@@ -2,7 +2,7 @@ pub mod implement;
 struct ProcessList(Box<[ProcessListEntry]>);
 struct ProcessListEntry {
     pub pid: u32,
-    pd: PlatformData,
+    //pd: PlatformData,
 }
 #[cfg(target_os = "macos")]
 struct PlatformData;
